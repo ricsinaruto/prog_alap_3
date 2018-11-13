ffc7dcc06b2b91b1078fb0c01113b1fe4afac07c
Richárd Krisztián Csáky <ricsinaruto@hotmail.com>
2018-11-13 10:00:32 +0100

    finished junit lab

--- JUnitLab/test/junitlab/bank/BankTestFixture.java
+++ JUnitLab/test/junitlab/bank/BankTestFixture.java
@@ -33,6 +33,27 @@ public class BankTestFixture {
 		assertEquals(bank.getBalance(acc_number2), 8544);
 	}
 	
+	@Test
+	public void testClose() throws AccountNotExistsException {
+		boolean closed = bank.closeAccount(acc_number1);
+		assertEquals(closed, false);
+		
+		String new_acc = bank.openAccount();
+		boolean closed2 = bank.closeAccount(new_acc);
+		assertEquals(closed2, true);
+		
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testNegDep() throws AccountNotExistsException {
+		bank.deposit(acc_number1, -1);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void testTransferError() throws AccountNotExistsException, NotEnoughFundsException {
+		bank.transfer(acc_number2, acc_number1, -1);
+	}
+	
 	@Test(expected=junitlab.bank.NotEnoughFundsException.class)
 	public void testTransferWithoutEnoughFunds() throws AccountNotExistsException, NotEnoughFundsException {
 		bank.transfer(acc_number1, acc_number2, 3456);
